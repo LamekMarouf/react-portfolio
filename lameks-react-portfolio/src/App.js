2240d08a8499ff11b8c39e5baf964b49903f339e
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-04-22 17:02:29 +1000

    added about page js

--- lameks-react-portfolio/src/App.js
+++ lameks-react-portfolio/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import './style.css'
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
-
+import About from "./components/About";
 
 function App() {
   return (
     <>
     <Navbar />
-    <Homepage />
+    {/* <Homepage /> */}
+    <About />
     </>
   )
 }
---
+++ lameks-react-portfolio/src/components/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function About() {
+    return (
+        <section id="about-link">
+        <div>
+            <div className="about-me-section">
+                <div className="about-me-3d">
+                    <div id="about-me-title">About Me</div>
+                </div>
+                <div id="about-me-content">
+                    <p>Hello World! I love and enjoy the process and creativity that comes with
+                        building websites and apps. I am a graduate of The University of Sydney and
+                        Trilogy’s Full-Stack Coding Bootcamp. During this course, I have acquired all
+                        necessary skills to build a full-stack website with languages such as HTML,
+                        CSS, JavaScript, React and many more listed below. I have had experience in
+                        building a website from scratch with other fellow web developers, so I
+                        understand the process and mindset behind building something from the
+                        bottom up as a team. I have also built a full-stack web application alone so I
+                        can further understand the process of being independent and relying on
+                        myself to achieve and accomplish what is needed to be done.
+                    </p>
+            </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default About;
