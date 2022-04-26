06982d0a38bef60d25d8cd53ed377e17a9f4a8b5
Lamek Marouf <lamekmaroufjob@hotmail.com>
2022-04-26 15:48:43 +1000

    added portfolio link

--- lameks-react-portfolio/src/components/About.js
+++ lameks-react-portfolio/src/components/About.js
@@ -1,49 +1,54 @@
 import React from "react";
 import Navbar from "./Navbar";
 import hackerAvatar from "../assets/images/hackerAvatar.png";
-import Footer from "./Footer"
+import Footer from "./Footer";
 
 function About() {
   return (
     <>
-    <Navbar />
-    <section id="about-link">
-      <div>
-        <div className="about-me-section">
-          <div className="about-me-3d">
-            <div id="about-me-title">About Me</div>
-          </div>
+      <Navbar />
+      <section id="about-link">
+        <div>
+          <div className="about-me-section">
+            <div className="about-me-3d">
+              <div id="about-me-title">About Me</div>
+            </div>
 
-          <div id="about-me-content">
-            <h1 className="about-image">
-              <img src={hackerAvatar} alt="Hacker Avatar" id="hacker-avatar" />
-            </h1>
+            <div id="about-me-content">
+              <h1 className="about-image">
+                <img
+                  src={hackerAvatar}
+                  alt="Hacker Avatar"
+                  id="hacker-avatar"
+                />
+              </h1>
 
-            <div>
-              <p>
-                Hello World! I love and enjoy the process and creativity that
-                comes with building websites and apps. I am a graduate of The
-                University of Sydney and Trilogy’s Full-Stack Coding Bootcamp.
-              </p>
-              <p>
-                During this course, I have acquired all necessary skills to
-                build a full-stack website with languages such as HTML, CSS,
-                JavaScript, React and many more listed below. I have had
-                experience in building a website from scratch with other fellow
-                web developers, so I understand the process and mindset behind
-                building something from the bottom up as a team.
-              </p>
-              <p>
-                I have also built a full-stack web application alone so I can
-                further understand the process of being independent and relying
-                on myself to achieve and accomplish what is needed to be done.
-              </p>
+              <div>
+                <p>
+                  Hello World! I love and enjoy the process and creativity that
+                  comes with building websites and apps. I am a graduate of The
+                  University of Sydney and Trilogy’s Full-Stack Coding Bootcamp.
+                </p>
+                <p>
+                  During this course, I have acquired all necessary skills to
+                  build a full-stack website with languages such as HTML, CSS,
+                  JavaScript, React and many more listed below. I have had
+                  experience in building a website from scratch with other
+                  fellow web developers, so I understand the process and mindset
+                  behind building something from the bottom up as a team.
+                </p>
+                <p>
+                  I have also built a full-stack web application alone so I can
+                  further understand the process of being independent and
+                  relying on myself to achieve and accomplish what is needed to
+                  be done.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <Footer />
+      </section>
+      <Footer />
     </>
   );
 }
--- lameks-react-portfolio/src/components/Footer.js
+++ lameks-react-portfolio/src/components/Footer.js
@@ -1,30 +1,41 @@
 import React from "react";
+import resume from "../assets/extras/codingResume.pdf"
 
 function Footer() {
   return (
     <>
       <section className="footer-container">
-        <div
-          href="https://github.com/LamekMarouf"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <a className="github">GITHUB</a>
+        <div>
+          <a
+            href="https://github.com/LamekMarouf"
+            target="_blank"
+            rel="noreferrer"
+            className="github"
+          >
+            GITHUB
+          </a>
         </div>
 
-        <div
-          href="https://www.linkedin.com/in/lamek-marouf/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <a className="linkedin">LINKEDIN</a>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/lamek-marouf/"
+            target="_blank"
+            rel="noreferrer"
+            className="linkedin"
+          >
+            LINKEDIN
+          </a>
         </div>
 
-        <div
-        href=""
-        target="_blank"
-        rel="noreferrer">
-          <a className="resume">RESUME</a>
+        <div>
+          <a
+            href={resume}
+            target="_blank"
+            rel="noreferrer"
+            className="resume"
+          >
+            RESUME
+          </a>
         </div>
       </section>
     </>
